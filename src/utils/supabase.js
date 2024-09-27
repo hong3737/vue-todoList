8c1dafc7f5d24c227074f973d0352045bfe664bd
hong3737 <qwert3748@gmail.com>
2024-09-27 17:10:45 +0900

    Vue 3 환경 변수 네이밍 변경

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VUE_APP_SUPABASE_URL;
-const supabaseKey = process.env.VUE_APP_SUPABASE_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
